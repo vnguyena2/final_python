60eacb55264fe16291e2c5edd0182075330b79e5
Andres <avalenzuelatamayo@gmail.com>
2020-05-05 16:54:56 -0700

    add multiple TICK suppoer

--- stock_list/stock.py
+++ stock_list/stock.py
@@ -9,6 +9,15 @@ def retrieve_tix(u_answer,tick_list):
 	else:
 		print("Did not find that TIX, please try again")
 	return u_answer
+#MULTIPLE TICK SUPPORT
+# def multiple_tix(m_stockList,tick_list):
+# 	valid_options = list()
+# 	for ticks in m_stockList:
+# 		if ticks in tick_list:
+# 			valid_options.append(ticks)
+# 		else:
+# 			print("Was not able to find {} ".format(ticks))
+# 	print("Valid options were ", valid_options)
 
 def extract_ticker_symbols(u_file,tix_symbols):
 	with open(u_file, newline='') as opencsv:
@@ -24,11 +33,20 @@ def extract_ticker_symbols(u_file,tix_symbols):
 def main():
 	quit_key = "done"
 	extract_ticker_symbols(csv_file,ticker_list)
+	#user_stocks = list()
 	while True:
 		#Allowing the user to pass multiple stock letters
-		user_stocks = list()
+		# stock_letters = input("Enter multiple ticks to search for ticker stock " + \
+		# 						"Enter 'done' to quit : ")
+		# multiple_stocks = stock_letters.split(",")
+		# for tick in multiple_stocks:
+		# 	user_stocks.append(tick)
+		# if multiple_stocks > 1:
+		# 	multiple_stocks(user_stocks,ticker_list)
+
 		stock_letters = input("Enter up to 4 letters to search for ticker stock " + \
 								"Enter 'done' to quit : ")
+
 		if stock_letters == quit_key:
 			quit()
 		try:
