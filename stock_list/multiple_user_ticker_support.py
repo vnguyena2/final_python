5747576065708050c0e6f2976556b47dc7c51447
Andres <avalenzuelatamayo@gmail.com>
2020-05-17 14:39:33 -0700

    fix_multiple_ticks

--- stock_list/multiple_user_ticker_support.py
+++ stock_list/multiple_user_ticker_support.py
@@ -1,26 +1,61 @@
+#Run this file inside the git repository git repro final python
+import os
+import csv
 
+#extracted and cleaned ticker list
+csv_ticker_list= list()
+#user current dir
+cwd = os.getcwd()
+#new company csv to read off of
+new_u_file = "complete_ticker_list.csv"
+#complete path file
+abs_file_path = os.path.join(cwd,new_u_file)
+
+def return_tickers_from_csv(ticker_list,tick_csv_file):
+	with open(tick_csv_file, newline='') as opencsv:
+		reader = csv.reader(opencsv,quotechar=',')
+		for row in reader:
+			for items in row[0:1]:
+				ticker_list.append(items)
+	print('Successfully created list')
+	return ticker_list
 
 
 #MULTIPLE TICK SUPPORT
-def multiple_tix(m_stockList,tick_list):
-	valid_options = list()
-	for ticks in m_stockList:
-		if ticks in tick_list:
+def return_multiple_tix(user_ticker_list,csv_tick_list,valid_options):
+	#This function returns the valid tick options after checking the complete_ticker_list.csv
+	for ticks in user_ticker_list:
+		if ticks in csv_tick_list:
+			print(ticks, " is valid appending to list")
 			valid_options.append(ticks)
 		else:
-			print("Was not able to find {} ".format(ticks))
-	print("Valid options were ", valid_options)
+			print("{} is NOT a valid ticker ".format(ticks))
+	#print("Valid options were ", valid_options)
+	return valid_options
 
 def main():
-	#user_stocks = list()
+	return_tickers_from_csv(csv_ticker_list,abs_file_path)
+	# print(csv_ticker_list)
+	#this list is used  append  multiple user tickers, it checks if the ticks are valid by seeing if
+	#they exist in csv_ticker_list, if the tick is valid it appends to a valid list,
+	user_ticks = list()
+	quit_key = ('done')
+	valid_options = list()
 	while True:
 		#Allowing the user to pass multiple stock letters
-		# stock_letters = input("Enter multiple ticks to search for ticker stock " + \
-		# 						"Enter 'done' to quit : ")
-		# multiple_stocks = stock_letters.split(",")
-		# for tick in multiple_stocks:
-		# 	user_stocks.append(tick)
-		# if multiple_stocks > 1:
-		# 	multiple_stocks(user_stocks,ticker_list)
+		tick_letters = input("Enter multiple ticks to search for valid ticker marks " + \
+								"Enter 'done' to quit : ")
+		multiple_ticks_list = tick_letters.split(",")
+		if tick_letters == quit_key:
+			break
+		if len(multiple_ticks_list) == 1:
+			print("only added one key")
+			break		
+		if len(multiple_ticks_list) > 1:
+			for tick in multiple_ticks_list:
+				user_ticks.append(tick)
+	return_multiple_tix(user_ticks,csv_ticker_list,valid_options)
+	print(valid_options)
+
 if __name__ == '__main__':
 	main()
